c93cc6b85c372cc46b81399da9449f2807a7d19f
matheuskiyoshi <matheuskiyoshi@gmail.com>
2023-12-13 17:16:25 -0300

    feat(models): add models note and user

---
+++ app/models/note.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+let noteSchema = new mongoose.Schema({
+    title: String,
+    body: String,
+    done: Boolean,
+    deadline: String,
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+})
+
+noteSchema.index({ title: 'text', body: 'text' });
+
+module.exports = mongoose.model('Note', noteSchema);
