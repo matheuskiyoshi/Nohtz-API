c2c4efd6ec707fa12d19f2b221b74f904abcedd7
matheuskiyoshi <matheuskiyoshi@gmail.com>
2023-12-14 15:19:12 -0300

    feat: adding cors and the notes router

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+require('./config/database');
+var cors = require('cors');
 
 var usersRouter = require('./app/routes/users');
+var notesRouter = require('./app/routes/notes');
 
 var app = express();
 
@@ -10,7 +13,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/users', usersRouter);
+app.use('/notes', notesRouter);
 
 module.exports = app;
