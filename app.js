9cdbc77a8241489b9e355096aebcf44444e0a9de
matheuskiyoshi <matheuskiyoshi@gmail.com>
2023-12-15 14:47:49 -0300

    feat(app): add swagger path in app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var logger = require('morgan');
 require('./config/database');
 var cors = require('cors');
+const swaggerUi = require("swagger-ui-express")
+const swaggerFile = require("./swagger_output.json")
 
 var usersRouter = require('./app/routes/users');
 var notesRouter = require('./app/routes/notes');
@@ -14,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
